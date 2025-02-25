3c88032414ac987ee304c09f2f43498a9840327d
jony <nazrul229502@gmail.com>
2025-02-26 01:30:12 +0600

    added login.js file

---
+++ login.js
@@ -0,0 +1,20 @@
+document.getElementById("login").addEventListener('click',
+    function(event){
+        event.preventDefault();
+            const acoountNum = document.getElementById("number").value;
+            const validPin = document.getElementById("pin").value;
+            const convertedValidPin =parseInt(validPin);
+            if(acoountNum.length==11){
+                if(convertedValidPin===1234){
+                    window.location.href="./main.html";
+                }
+                else{
+                    alert('Invalid PIN');
+                }
+            }
+            else{
+                alert('Invalid Account Number');
+            }
+            
+})
+
